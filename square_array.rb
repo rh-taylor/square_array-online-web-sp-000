8bc5e017f598b0c5d329d3e18948ff86c945c7d6
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-29 18:06:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   integers = [1, 2, 3]
   squared integers = integers.each do|integers|
-    puts "#{integers}**2"
+    puts "#{}integers**2"
   end
   
   return squared_integers
